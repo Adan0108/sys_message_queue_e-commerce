d3e198a5ac98f0819ab62112b817d019aabd07dd
Quang Anh <lequanganh123@gmail.com>
2025-04-24 16:09:00 +1000

    Process message when  MQ server die (TTL)

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 'use strict'
 
-const { consumerToQueue } = require('./src/services/consumerQueue.service')
+const { consumerToQueue,consumerToQueueFail,consumerToQueueNormal } = require('./src/services/consumerQueue.service')
 const queueName = 'test-topic'
 
-consumerToQueue(queueName).then(() => {
-  console.log(`Consumer started for queue: ${queueName}`)
+// consumerToQueue(queueName).then(() => {
+//   console.log(`Consumer started for queue: ${queueName}`)
+// }).catch( err => {
+//   console.error('Error starting consumer:', err)
+// })
+
+consumerToQueueNormal(queueName).then(() => {
+  console.log(`Consumer started for consumerToQueueNormal`)
+}).catch( err => {
+  console.error('Error starting consumer:', err)
+})
+
+consumerToQueueFail(queueName).then(() => {
+  console.log(`Consumer started for consumerToQueueFail`)
 }).catch( err => {
   console.error('Error starting consumer:', err)
 })
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -5,6 +5,11 @@ const {
   connectToRabbitMQ
 } = require('../dbs/init.rabbit')
 
+const log = console.log
+console.log = function(){
+  log.apply(console, [new Date()].concat(arguments))
+}
+
 const messageService = {
   consumerToQueue: async (queueName) => {
     try {
@@ -13,6 +18,53 @@ const messageService = {
     } catch (error) {
       console.error('Error in consumerToQueue:', error)
       
+    }
+  },
+  //case processing
+  consumerToQueueNormal : async(queueName) => {
+    try {
+      const { channel , connection } = await connectToRabbitMQ()
+      const notiQueue = 'notificationQueueProcess' //assrtQueue
+
+      const timeExpired = 15000
+      setTimeout(() => {
+        channel.consume(notiQueue, msg =>{
+          console.log(`Send notificationQUeue Success::` , msg.content.toString())
+          channel.ack(msg)
+        })
+      } , timeExpired)
+
+    } catch (error) {
+      console.error('Error in consumerToQueueNormal:', error)
+    }
+  },
+  //case failed processing
+  consumerToQueueFail : async(queueName) => {
+    try {
+      const { channel , connection } = await connectToRabbitMQ()
+
+      const notificationExchangeDLX = 'notificationExDLX' //notificatio direct\
+      const notificationRoutingKeyDLX = 'notificationRoutingKeyDLX' //assert
+
+      const notiQueueHandler = 'notificationQueueNotFix'
+      await channel.assertExchange(notificationExchangeDLX, 'direct', {
+        durable: true
+      })
+      
+
+      const queueResult = await channel.assertQueue(notiQueueHandler, { 
+        exclusive: false, //cho phep cac ket noi try cap vao cung luc hang doi
+      });
+
+      await channel.bindQueue(queueResult.queue, notificationExchangeDLX, notificationRoutingKeyDLX)
+      await channel.consume(queueResult.queue, msgFail =>{
+        console.log(`Send notificationQUeue Fail , please hot fix::` , msgFail.content.toString())
+      }, {
+        noAck: true // Acknowledge the message automatically ( if send then dont resend)
+      })
+    } catch (error) {
+      console.error('Error in consumerToQueueFail:', error)
+      throw error
     }
   }
 }
